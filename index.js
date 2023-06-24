306e150c4c0ec45b22b1d2e944dad738d5bcd70d
priyankasinghhhhh <109071543+priyankasinghhhhh@users.noreply.github.com>
2023-06-24 15:13:46 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var Name = "Uday Gupta";
+var roll = "201570013";
+var date = new Date().toLocaleDateString();
+var clg = "GLA University, Mathura";
+
+
+document.getElementById('name').innerHTML = Name;
+document.getElementById('roll').innerHTML = roll;
+document.getElementById('clg').innerHTML = clg;
+document.getElementById('date').innerHTML = date;
